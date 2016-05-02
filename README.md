7dae5e34e4e631382ed3569672b3735c96327745
Math-ias <mathi4semail@gmail.com>
2016-05-02 17:04:45 -0400

    Create python script to update pastebin with newest installer code.
    This python script, which requires the use of a pastebin api code, creates a paste on pastebin with
    the newest installer. It updates the README.md with this new pastebin code. It runs on python3, python2
    is untested.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # VimCC
+
+The current pastebin code: `HiD53ugy`
+
 **By default `tab` is binded to exit insert mode, this can be rebound in the vimrc!**
 
 Dissatisfied with the existing editor in ComputerCraft (seriously, try using it 
@@ -20,7 +23,7 @@ The installer is both available as part of this [repo](./installer),
 and as a download from pastebin, which can be directly gotten in the game by
 running the following command in a ComputerCraft terminal: 
 
-	pastebin run j1NxmAzb
+	pastebin run [pastebin code]
 
 ### Manually
 download all the files from here.
---
+++ upload.py
@@ -0,0 +1,31 @@
+import re
+import json
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+readmetext = None
+
+readme = open("README.md", "r")
+readmetext = readme.read()
+readme.close()
+
+with open("PASTEBIN_API_KEY", "r") as key, open("installer", "r") as installer, open("README.md", "w") as readme:
+	key = key.read()
+	values = {
+		'api_dev_key': key.strip(),
+		'api_option': 'paste',
+		'api_paste_code': installer.read(),
+		'api_paste_format': 'lua',
+	}
+	url = "http://pastebin.com/api/api_post.php"
+	data = urlencode(values)
+	data = data.encode('utf-8')
+	req = Request(url, data)
+	with urlopen(req) as resp:
+		response = resp.read().decode('utf-8').strip()
+		if response[:1] == "B":
+			print('Bad request, upload failed.')
+			print(response)
+			exit()
+		code = response[20:]
+		readme.write(re.sub("The current pastebin code: `\w+`", "The current pastebin code: `" + code + "`", readmetext))
